7546f7ee41f3c9a13f5748067498a269a46acb0b
Thijs <r0793167@student.thomasmore.be>
2022-03-14 16:15:17 +0100

    Contact form order

--- components/sections/contact/Contact.tsx
+++ components/sections/contact/Contact.tsx
@@ -8,15 +8,21 @@ function Contact() {
     <>
         <div className="grid grid-cols-1 md:grid-cols-2">
           <div>
-            <p className="text-gray-light font-semibold">Contact</p>
-            <p className="text-3xl md:text-4xl font-black text-blueish">Vraag uw offerte aan of <span className="hidden xl:inline"><br/></span> stel een vraag</p>
+          <p className="text-gray-light font-semibold">Contact</p>
+          <p className="text-3xl md:text-4xl font-black text-blueish">Vraag uw offerte aan of <span className="hidden xl:inline"><br/></span> stel een vraag</p>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <div className="order-1">
             <div className="mt-6">
               <Info img={"/contact/phone.svg"} text={"+32 471 25 39 84"}></Info>
               <Info img={"/contact/mail.svg"} text={"thijs.wouters@outlook.com"}></Info>
               <Info img={"/contact/website.svg"} text={"boonhofstraat 54, 2480 Dessel"}></Info>
             </div>
           </div>
-          <Form></Form>
+          <div className="order-0 md:order-1 mt-0 md:-mt-24">
+            <Form></Form>
+          </div>
         </div>
     </>
   );
