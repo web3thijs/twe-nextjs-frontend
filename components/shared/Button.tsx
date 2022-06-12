df0fb56d3c2673d800890bc3d0995a4230cab1eb
Thijs <r0793167@student.thomasmore.be>
2022-06-12 16:09:28 +0200

    Added type & callback

--- components/shared/Button.tsx
+++ components/shared/Button.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { ButtonHTMLAttributes, MouseEventHandler } from "react";
 
 type ButtonProps = {
     text: string;
     outline: boolean;
+    type?: ButtonHTMLAttributes<HTMLButtonElement>
+    callback?: MouseEventHandler<HTMLButtonElement>
     className?: string;
 }
 
-function Button({text, outline, className}: ButtonProps) {
+function Button({text, outline, className, callback}: ButtonProps) {
     if(outline == false){
-        return <button type="button" className={`text-base text-center text-white bg-greenish font-semibold border-solid border-2 border-greenish rounded-lg px-5 py-2 ${ className }`}>{ text }</button>
+        return <button type="button" onClick={callback} className={`text-base text-center text-white bg-greenish font-bold border-solid border-2 border-greenish rounded-lg px-5 py-2 ${ className }`}>{ text }</button>
     } else {
         return <button type="button" className="text-base text-center text-greenish font-semibold border-solid border-2 border-greenish rounded-lg px-5 py-2">{ text }</button>
     }
