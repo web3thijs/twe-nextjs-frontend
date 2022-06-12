fd302d5ac64ffe18fcc681250e6861c547cb6e98
Thijs <r0793167@student.thomasmore.be>
2022-06-12 17:38:47 +0200

    Fixed links

--- components/core/Navbar.tsx
+++ components/core/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar(){
             <div className="container flex flex-wrap justify-between items-center mx-auto">
                 <a href="#" className="flex items-center">
                     <img src="logo.svg" className="mr-3 h-6 sm:h-10" alt="Flowbite Logo" />
-                    <span className="self-center text-xl font-bold text-blueish whitespace-nowrap dark:text-white">TWE</span>
+                    <span className="self-center text-xl font-bold text-blueish whitespace-nowrap">TWE</span>
                 </a>
                 <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                 <span className="sr-only">Open main menu</span>
@@ -14,21 +14,21 @@ export default function Navbar(){
                 <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 </button>
                 <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
-                <ul className="flex">
+                <ul className="flex font-semibold">
                     <li className="ml-14">
-                        <a href="#" className="text-greenish">Home</a>
+                        <a href="#header" className="text-blueish">Home</a>
                     </li>
                     <li className="ml-14">
-                        <a href="#" className="text-blueish">Diensten</a>
+                        <a href="#services" className="text-blueish">Diensten</a>
                     </li>
                     <li className="ml-14">
-                        <a href="#" className="text-blueish">Contact</a>
+                        <a href="#contact" className="text-blueish">Contact</a>
                     </li>
                     <li className="ml-14">
-                        <a href="#" className="text-blueish">Realisaties</a>
+                        <a href="#references" className="text-blueish">Realisaties</a>
                     </li>
                     <li className="ml-14">
-                        <a href="#" className="text-blueish">Reviews</a>
+                        <a href="#reviews" className="text-blueish">Reviews</a>
                     </li>
                 </ul>
                 </div>
