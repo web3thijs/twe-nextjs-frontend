1b36c6639d4c06dab763987ed6dce83fe87a9e3f
Thijs <r0793167@student.thomasmore.be>
2022-06-13 15:49:17 +0200

    Remove nav logo text

--- components/core/Navbar.tsx
+++ components/core/Navbar.tsx
@@ -6,7 +6,6 @@ export default function Navbar(){
             <div className="container flex flex-wrap justify-between items-center mx-auto">
                 <a href="#" className="flex items-center">
                     <img src="logo.svg" className="mr-3 h-6 sm:h-10" alt="Flowbite Logo" />
-                    <span className="self-center text-xl font-bold text-blueish whitespace-nowrap">TWE</span>
                 </a>
                 <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                 <span className="sr-only">Open main menu</span>
