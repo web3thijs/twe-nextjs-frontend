2d6f05b3a97835d394f2786b012f174b4346efc9
Thijs <r0793167@student.thomasmore.be>
2022-06-13 17:35:58 +0200

    Code align & hide on small

--- components/sections/services/Services.tsx
+++ components/sections/services/Services.tsx
@@ -11,27 +11,45 @@ function Services() {
             <div className="grid grid-cols-1 sm:grid-cols-3 md:mt-16">
               <Column img={"/services/category_1.svg"} title={"Nieuwbouw"}
                 rows={[
-                  'Totaalproject', 'Verdeelkasten', 'Laadpalen', 'Domotica',
-                  'Afwerking', 'Armaturen', 'Slijpwerken', 'Datavoorziening',
-                  'Bedrijfspanden', 'Bedrijfshallen'
+                  'Totaalproject',
+                  'Verdeelkasten',
+                  'Laadpalen',
+                  'Domotica',
+                  'Afwerking',
+                  'Armaturen',
+                  'Slijpwerken',
+                  'Datavoorziening',
+                  'Bedrijfspanden',
+                  'Bedrijfshallen'
               ]}></Column>
 
               <Column img={"/services/category_2.svg"} title={"Renovaties"}
                 rows={[
-                  'Totaalproject', 'Bekabeling', 'Aarding',
-                  'Schema´s', 'Datavoorziening',
-                  'Bedrijfspanden', 'Bedrijfshallen'
+                  'Totaalproject',
+                  'Bekabeling',
+                  'Aarding',
+                  'Schema´s',
+                  'Datavoorziening',
+                  'Bedrijfspanden',
+                  'Bedrijfshallen'
               ]}></Column>
             
               <Column img={"/services/category_3.svg"} title={"Klusjes"}
                 rows={[
-                  'Aarding', 'Aanpassingen', 'Verlichting',
-                  'Stopcontacten', 'Foutopsporing', 'Tuinoverkapping', 'Tuinhuis'
+                  'Aarding',
+                  'Aanpassingen',
+                  'Verlichting',
+                  'Stopcontacten',
+                  'Foutopsporing',
+                  'Tuinoverkapping',
+                  'Tuinhuis'
               ]}></Column>
             </div>
             
             <div className="flex justify-center mt-16">
-              <Image alt={'Not found'} src={'/services/not_found.svg'} width={75} height={75}/>
+              <div className="hidden sm:block">
+                <Image alt={'Not found'} src={'/services/not_found.svg'} width={75} height={75}/>
+              </div>
               <div className="text-left my-auto ml-6">
                 <p className="text-blueish font-bold">Niet gevonden waar u naar op zoek bent?</p>
                 <p className="text-gray">Contacteer ons om te bespreken wat er verder nog mogelijk is.</p>
