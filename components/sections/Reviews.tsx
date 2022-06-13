fe15cebbc2b3f217747bcced803b321ce652f818
Thijs <r0793167@student.thomasmore.be>
2022-06-13 17:36:05 +0200

    Responsive

--- components/sections/Reviews.tsx
+++ components/sections/Reviews.tsx
@@ -5,27 +5,31 @@ import Image from 'next/image'
 function Reviews() {
   return (
     <>
-        <div className="grid md:grid-cols-2">
+        <div className="grid sm:grid-cols-2">
             <div>
               <Title text="Reviews" row1={"Wat vonden de klanten"} row2={"van onze service?"}></Title>
 
               <div className="mt-6 flex content-center">
-                <Image alt="Facebook logo" src={'/reviews/facebook.svg'} layout="fixed" height={48} width={48} objectFit="cover"/>
+                <div className="relative w-8 h-8">
+                  <Image alt="Facebook logo" src={'/reviews/facebook.svg'} layout="fill" objectFit="cover"/>
+                </div>
                 <div className="flex my-auto ml-5">
                   {[...Array(5)].map((x, i) =>
-                    <div key={i} className={'ml-2'}>
-                      <Image alt="Star filled" src={'/reviews/star-fill.svg'} layout="fixed" height={28} width={28} objectFit="cover"/>
+                    <div key={i} className={'relative ml-2 w-6 h-6'}>
+                      <Image alt="Star filled" src={'/reviews/star-fill.svg'} layout="fill" objectFit="cover"/>
                     </div>
                   )}
                 </div>
               </div>
 
               <div className="mt-6 flex content-center">
-                <Image alt="Google logo" src={'/reviews/google.svg'} layout="fixed" height={48} width={48} objectFit="cover"/>
+                <div className="relative w-8 h-8">
+                  <Image alt="Google logo" src={'/reviews/google.svg'} layout="fill" objectFit="cover"/>
+                </div>
                 <div className="flex my-auto ml-5">
                   {[...Array(5)].map((x, i) =>
-                    <div key={i} className={'ml-2'}>
-                      <Image alt="Star filled" src={'/reviews/star-fill.svg'} layout="fixed" height={28} width={28} objectFit="cover"/>
+                    <div key={i} className={'relative ml-2 w-6 h-6'}>
+                      <Image alt="Star filled" src={'/reviews/star-fill.svg'} layout="fill" objectFit="cover"/>
                     </div>
                   )}
                 </div>
