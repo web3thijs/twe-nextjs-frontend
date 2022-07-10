0bd666c0884d347cae8712c7582eb2a29316dffb
Thijs <r0793167@student.thomasmore.be>
2022-07-10 18:02:22 +0200

    references layout

--- components/sections/references/References.tsx
+++ components/sections/references/References.tsx
@@ -1,29 +1,28 @@
 import Title from "../../shared/Title";
 import Project from "./Project";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronRight, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChevronRight,
+  faChevronLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import Button from "../../shared/Button";
 
 function References() {
   return (
     <>
-        <div className="">
-            <div className="grid grid-cols-2">
-              <div>
-                <Title text={"Realisaties"} row1={"Recente projecten"}></Title>
-              </div>
-              <div className="text-right mt-8 text-2xl text-gray">
-                <FontAwesomeIcon icon={faChevronLeft} className={'mr-5 text-gray-light'}/>
-                <FontAwesomeIcon icon={faChevronRight}/>
-              </div>
-            </div>
-            <div className="flex mt-8">
-              <Project img={"/header/one.jpg"}></Project>
-              <Project img={"/header/two.jpg"}></Project>
-              <Project img={"/header/three.jpg"}></Project>
-              <Project img={"/header/one.jpg"}></Project>
-              <Project img={"/header/two.jpg"}></Project>
-            </div>
+      <div className="">
+        <div className="text-center">
+          <Title text={"Realisaties"} row1={"Recente projecten"}></Title>
         </div>
+        <div className="flex justify-center space-x-12 my-16">
+          <Project img={"/header/one.jpg"}></Project>
+          <Project img={"/header/two.jpg"}></Project>
+          <Project img={"/header/three.jpg"}></Project>
+        </div>
+        <div className="flex justify-center">
+          <Button text={"View more"} outline={true}></Button>
+        </div>
+      </div>
     </>
   );
 }
