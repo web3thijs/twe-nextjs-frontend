ba3776bf5eaea3b1d94af4828c34b45dec838bfb
Thijs <r0793167@student.thomasmore.be>
2022-07-10 18:02:03 +0200

    Project card

--- components/sections/references/Project.tsx
+++ components/sections/references/Project.tsx
@@ -1,16 +1,31 @@
 import React from "react";
-import Image from "next/image"
+import Image from "next/image";
 
 type ProjectProps = {
-    img: string;
-}
+  img: string;
+};
 
-function Project({img}: ProjectProps) {
-    return <>
-        <div className="relative z-0 h-96 w-60 mr-5">
-            <Image alt={'title'} src={img} layout={'fill'} priority className="rounded-lg" objectFit="cover"/>
+function Project({ img }: ProjectProps) {
+  return (
+    <>
+      <div className="bg-white rounded-xl shadow-3xl">
+        <div className="relative z-0 h-64 w-80">
+          <Image
+            alt={"title"}
+            src={img}
+            layout={"fill"}
+            priority
+            className="rounded-t-xl"
+            objectFit="cover"
+          />
         </div>
+        <div className={"px-6 py-5"}>
+          <p className="font-semibold text-blueish">Tuinhuis</p>
+          <p className="text-gray">Wij hebben dit tuinhuis...</p>
+        </div>
+      </div>
     </>
+  );
 }
 
 export default Project;
