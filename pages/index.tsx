af0e74297ae5ee932bb37dd3b3b4de6287513466
Thijs <r0793167@student.thomasmore.be>
2022-07-17 16:32:28 +0200

    colors & paddings for sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import Contact from "../components/sections/contact/Contact";
-import Header from "../components/sections/Header";
-import References from "../components/sections/references/References";
-import Reviews from "../components/sections/Reviews";
-import Services from "../components/sections/services/Services";
+import Contact from "../components/sections/3_contact/Contact";
+import Header from "../components/sections/1_header/Header";
+import References from "../components/sections/4_references/References";
+import Reviews from "../components/sections/5_reviews/Reviews";
+import Services from "../components/sections/2_services/Services";
 
 const Home: NextPage = () => {
   return (
@@ -14,27 +14,25 @@ const Home: NextPage = () => {
         <title>TW Elektriciteitswerken</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <section className="mt-20" id="header">
+      <section className="pt-32 pb-48" id="header">
         <Header></Header>
       </section>
 
-      <section className="mt-20" id="services">
+      <section className="bg-offwhite py-32" id="services">
         <Services></Services>
       </section>
 
-      <section className="mt-20" id="contact">
+      <section className="py-32" id="contact">
         <Contact></Contact>
       </section>
 
-      <section className="mt-20" id="references">
+      <section className="bg-offwhite py-32" id="references">
         <References></References>
       </section>
 
-      <section className="mt-20" id="reviews">
+      <section className="py-32" id="reviews">
         <Reviews></Reviews>
       </section>
-
-      <section className="mt-20" id="footer"></section>
     </>
   );
 };
