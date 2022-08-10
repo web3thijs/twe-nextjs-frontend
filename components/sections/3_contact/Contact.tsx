55191655abedc4d419b29f4a5845d03681939e3e
Thijs <r0793167@student.thomasmore.be>
2022-08-10 13:53:11 +0200

    Contact update

--- components/sections/3_contact/Contact.tsx
+++ components/sections/3_contact/Contact.tsx
@@ -5,6 +5,8 @@ import {
   faGlobe,
   faGlobeEurope,
   faInfoCircle,
+  faLocation,
+  faLocationArrow,
   faPhone,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -23,31 +25,78 @@ function Contact() {
         ></Title>
       </div>
       <div className="grid grid-cols-1">
-        <div className="mt-6 order-1">
+        <div className="mt-12 order-0 md:order-1">
           <div className="max-w-4xl mx-auto">
             <Form></Form>
           </div>
         </div>
         <div>
-          <div className="mt-6 flex space-x-6 justify-center">
-            <a href="tel: +32471 25 39 84"className="flex bg-white border border-solid border-gray-light rounded-xl w-fit py-5 px-7 shadow-3xl">
+          <div className="mt-12 grid grid-cols-1 md:grid-cols-3 justify-center max-w-3xl mx-auto">
+            <div className="flex">
+              <a
+                href="mailto: info@tw-elek.be"
+                className="grid grid-rows-2 mx-auto"
+              >
+                <FontAwesomeIcon
+                  icon={faEnvelope as IconProp}
+                  className={"text-blueish text-greenish my-auto mx-auto"}
+                  size={'2x'}
+                />
+                <span className="ml-3 my-auto text-gray mt-2">
+                  info@tw-elek.be
+                </span>
+              </a>
+            </div>
+            <div className="flex mt-10 md:mt-0">
+              <a
+                href="tel: +32471 25 39 84"
+                className="grid grid-rows-2 mx-auto"
+              >
+                <FontAwesomeIcon
+                  icon={faPhone as IconProp}
+                  className={"text-blueish text-greenish my-auto mx-auto"}
+                  size={'2x'}
+                />
+                <span className="ml-3 my-auto text-gray mt-2">
+                  +32 471 25 39 84
+                </span>
+              </a>
+            </div>
+            <div className="flex mt-10 md:mt-0">
+              <a
+                href="https://www.google.com/maps/place/TW+Elektriciteitswerken/@51.2420141,5.1175145,17z/data=!4m5!3m4!1s0x47c6cbe8cf8533e3:0xf48f5de54e148f81!8m2!3d51.2420108!4d5.1196978"
+                className="grid grid-rows-2 mx-auto"
+              >
+                <FontAwesomeIcon
+                  icon={faLocationArrow as IconProp}
+                  className={"text-blueish text-greenish my-auto mx-auto"}
+                  size={'2x'}
+                />
+                <span className="ml-3 my-auto text-gray mt-2">
+                  Boonhofstraat 54, Dessel
+                </span>
+              </a>
+            </div>
+            {/* <a
+              href="tel: +32471 25 39 84"
+              className="flex bg-white border border-solid border-gray-light rounded-xl w-full sm:w-11/12 py-5 px-7 shadow-3xl"
+            >
               <FontAwesomeIcon
                 icon={faPhone as IconProp}
-                className={"text-blueish"}
+                className={"text-blueish text-greenish my-auto mx-3"}
               />
+              <span className="ml-3 my-auto text-gray ">+32 471 25 39 84</span>
             </a>
-            <a href="mailto: twelektriciteitswerken@outlook.com" className="flex bg-white border border-solid border-gray-light rounded-xl w-fit py-5 px-7 shadow-3xl">
+            <a
+              href="mailto: twelektriciteitswerken@outlook.com"
+              className="flex bg-white border border-solid border-gray-light rounded-xl w-full sm:sm:w-11/12 py-5 px-7 shadow-3xl"
+            >
               <FontAwesomeIcon
                 icon={faEnvelope as IconProp}
-                className={"text-blueish"}
+                className={"text-blueish text-greenish my-auto mx-3"}
               />
-            </a>
-            <a href="https://www.facebook.com/TWelektriciteitswerken" target={'_blank'} rel="noreferrer" className="flex bg-white border border-solid border-gray-light rounded-xl w-fit  py-5 px-7 shadow-3xl">
-              <FontAwesomeIcon
-                icon={faGlobeEurope as IconProp}
-                className={"text-blueish"}
-              />
-            </a>
+              <span className="ml-4 my-auto text-gray">info@tw-elek.be</span>
+            </a> */}
           </div>
         </div>
       </div>
